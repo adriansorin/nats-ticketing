91c7f3710a14b02a8af1ffa3baaa8ba0583542fd
Adrian Avram <adrian.avram@talkspace.com>
2021-03-13 16:27:58 +0200

    change base ingress url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'https://ingress-nginx-controller-admission.ingress-nginx.svc',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
